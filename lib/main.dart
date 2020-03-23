ad19ff0df815fc9ddc51409be9c8938e865937a7
saulogp <sauloproetti@gmail.com>
2020-03-23 20:42:23 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:pega_gif/ui/homepage.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white,
+    ),
+
+  ));
+}
